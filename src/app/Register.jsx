2a951937e9ef7d294411766c4f33d73d2570130f
adem <adem@DESKTOP-SEV358T>
2025-11-03 23:53:12 +0100

    moved data to firebase

--- src/app/Register.jsx
+++ src/app/Register.jsx
@@ -94,7 +94,7 @@ const Register = () => {
         <div className="text-center mb-8">
           <div className="inline-flex items-center justify-center w-16 h-16 -gradient-to-br bgfrom-teal-500 to-emerald-600 rounded-2xl mb-4 shadow-lg">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-8 h-8">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 14v7m0 0h7m-7 0H5m7-7a7 7 0 100-14 7 7 0 000 14z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
             </svg>
           </div>
           <h1 className="text-3xl font-bold text-gray-900 mb-2">Create an Account</h1>
