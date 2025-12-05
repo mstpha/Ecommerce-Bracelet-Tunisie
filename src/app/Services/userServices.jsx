59faafd5139f3015fe52acd5da880c0968deac1b
adem <adem@DESKTOP-SEV358T>
2025-12-06 00:27:13 +0100

    fix error

--- src/app/Services/userServices.jsx
+++ src/app/Services/userServices.jsx
@@ -1,6 +1,6 @@
 import toast from "react-hot-toast";
 
-const API_URL = 'http://localhost:5000/api'
+const API_URL = 'https://ecommerce-bracelet-tunisie-backend.onrender.com/api';
 
 //
 // Token management
